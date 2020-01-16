143f2849faaa8dffd6b19eb5e007b3f84b620bf7
mateata <kayanepsu@gmail.com>
2020-01-17 00:49:42 +0900

    - 파일 정리

--- app/src/main/java/con/jwlee/itsmap/ui/home/HomeFragment.kt
+++ app/src/main/java/con/jwlee/itsmap/ui/home/HomeFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import con.jwlee.itsmap.R
 import con.jwlee.itsmap.ui.BaseActivity
 import con.jwlee.itsmap.ui.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : BaseFragment() {
+
+    var mapYn = false
 
 
     override fun onCreateView(
@@ -16,7 +20,7 @@ class HomeFragment : BaseFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(con.jwlee.itsmap.R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
         return root
     }
 
@@ -33,8 +37,6 @@ class HomeFragment : BaseFragment() {
     }
 
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init(view)
@@ -43,6 +45,14 @@ class HomeFragment : BaseFragment() {
 
 
     fun init(root : View) {
-
+        map_temp.setOnClickListener {
+            if(mapYn) {
+                map_temp.setBackgroundResource(R.drawable.map_temp_sw)
+                mapYn = false
+            } else {
+                map_temp.setBackgroundResource(R.drawable.map_temp_click)
+                mapYn = true
+            }
+        }
     }
 }
